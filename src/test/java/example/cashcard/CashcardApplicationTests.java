c8d2d095f6d8f1a96fca42fd6a789c1b118d97e6
Dani <danielmunozgil@gmail.com>
2023-08-24 10:47:05 +0200

    Test for the GET Endpoint

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -1,7 +1,7 @@
 package example.cashcard;
 
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
+//import com.jayway.jsonpath.DocumentContext;
+//import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
